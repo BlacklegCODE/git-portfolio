b39a8003dfb34e67c4d4ef42b60e107e56f78f5e
BlacklegCODE <dkingston0000@gmail.com>
2024-10-25 21:21:23 +0530

    Create Bubblesort.c

---
+++ Bubblesort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+
+    // Ask the user for the size of the array
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    // Input the elements of the array
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble sort
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Print the sorted array
+    printf("Sorted array:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
